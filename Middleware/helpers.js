d4eb14be83494acf0858aaf588315d972aa3cb36
Webudvikler Hovedforløb <heka@techcollege.dk>
2024-01-19 12:08:55 +0100

    Add random order list handlers

--- Middleware/helpers.js
+++ Middleware/helpers.js
@@ -1,4 +1,5 @@
 import { query } from "express"
+import { Sequelize } from "sequelize"
 
 /**
  * 
@@ -8,8 +9,14 @@ import { query } from "express"
  */
 export const QueryParamsHandle = (req, default_attr) => {
 	const attr = (req.query.attributes) ? req.query.attributes.trim() : default_attr.trim()
+
+	// Sætter sorteringsnøgle (sort_key) til id hvis false
+	// Hvis random kaldes random sortering via Sequelize
 	const query_params = {
-		sort_key: [req.query.sort_key || 'id'],
+		sort_key: [
+			!req.query.sort_key ? 'id' : 
+				req.query.sort_key === 'random' ? Sequelize.literal('rand()') : req.query.sort_key
+		],
 		limit: Number(req.query.limit) || 10000000,
 		attributes: attr.split(',').map(str => str.trim())
 	}
